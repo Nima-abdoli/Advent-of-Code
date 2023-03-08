651630cbdbaec22d2ae96f2e54924321b4abdce5
Nima abdoli <122664866+MinkStudio@users.noreply.github.com>
2023-03-08 22:01:03 +0330

    Day1 (C-Sharp)
    this is day one challenge. complete

---
+++ C#/Day1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace day1{
+    static public class program{
+
+        
+
+        static void Main()
+        {
+            int count = 0;
+            //int startfrom = 0;
+            int NowSum = 0;
+            int PastSum = 0;
+
+            string[] text = File.ReadAllLines(@"C:\Users\Xmasta\Desktop\Advent of Code\Day1\input2.txt");
+
+            System.Console.WriteLine(" text "+text.Length);
+
+            int[] input = new int[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                input[i] = int.Parse(text[i]);
+            }
+
+            for (int i = 0; i < input.Length-2; i++)
+            {
+                NowSum = input[i] + input[i+1] + input[i+2]; 
+
+                if (PastSum < NowSum && PastSum !=0)
+                {
+                    count++;
+                    PastSum = NowSum;
+                }else
+                {
+                    PastSum = NowSum;
+                }
+                
+            }
+            
+
+            System.Console.WriteLine(count);
+        }
+    }
+}
+
+
