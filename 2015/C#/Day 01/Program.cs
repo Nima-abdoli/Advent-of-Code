904110073868dc7e3f4ca8beab57790040dd4700
Nima abdoli <nimaabdoli@outlook.com>
2023-11-29 22:13:35 +0330

    Day 01 - Part 1 - 2015

---
+++ 2015/C#/Day 01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input;
+            int floor = 0;
+            string inputtext;
+            string filepath ="Input Day01-1.txt";
+
+            Console.WriteLine("Reading input ...");
+            inputtext = File.ReadAllText(filepath);
+
+            input = inputtext.ToCharArray();
+
+            foreach (char i in input)
+            {
+                if (i == '(')
+                {
+                    floor++;
+                }else
+                {
+                    floor--;
+                }
+            }
+
+            Console.WriteLine("Santa is in Floor : {0}",floor); // Answer is '138'
+        }
+    }
+}
